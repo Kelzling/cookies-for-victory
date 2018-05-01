7b718385ed3a04a145161969f8e2247535c30de3
Kelsey <kelzling@gmail.com>
2018-05-01 21:58:22 +1200

    update Goal to standardjs

--- src/Goal.js
+++ src/Goal.js
@@ -1,20 +1,25 @@
+/* Created by Kelsey Vavasour and Thomas Baines April 2018
+Conforms to StandardJS 01/05/2018 */
+
+/* global State, Vec */
+
 class Goal {
   constructor (pos) {
     this.pos = pos
   }
-  
+
   get type () {
     return 'goal'
   }
-  
+
   static create (pos) {
     return new Goal(pos)
   }
-  
+
   collide (state) {
     return new State(state.level, state.actors, 'won')
   }
-  
+
   update () {
     return this
   }
