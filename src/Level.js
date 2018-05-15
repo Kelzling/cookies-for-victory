bcd0ec70ffff853926b785b70d186ce4f320d458
Hakura77 <36619964+Hakura77@users.noreply.github.com>
2018-05-16 08:51:48 +1200

    update for standardJS

--- src/Level.js
+++ src/Level.js
@@ -2,7 +2,7 @@
 Refactored and Modified by Kelsey Vavasour and Thomas Baines April 2018
 Conforms to StandardJS 16/05/2018 */
 
-/* global Vec, Lava, Player, Coin, Heart, Goal, Checkpoint */
+/* global Vec, Lava, Player, Coin, Heart, Goal, Checkpoint, VERBOSE */
 
 class Level { // eslint-disable-line no-unused-vars
   constructor (plan) {
@@ -41,42 +41,41 @@ class Level { // eslint-disable-line no-unused-vars
     }
     return false
   }
-  
-  countActors(type) {
-    // counts the number of 
-    let numActors = this.startActors.filter(a => a.type == type).length
+
+  countActors (type) {
+    // counts the number of
+    let numActors = this.startActors.filter(a => a.type === type).length
     if (VERBOSE) {
       console.log(`There are ${numActors} actors of type: ${type} in this level.`)
     }
     return numActors
   }
-  
-  validate() {
+
+  validate () {
   // level validation - Checks a level for instances of specific objects
-  
+
     if (this.countActors('player') === 0) {
       console.warn('Critical Error: Level has no Player!') // critical error
       throw new ReferenceError('No Players in level!') // catch the no player problem before level is created
     } else if (VERBOSE) {
       console.log('Player detected')
     }
-    
-    
+
     console.assert(this.countActors('goal') > 0, 'Warning! This level has no goal!')
-    
+
     /* if (this.countActors('goal') === 0) { // goal check
         console.warn('Warning! This level has no goal!') // this is a critical error, so the warning comes through irrespective of VERBOSE
     } else if (VERBOSE) {
         console.log('Goal(s) detected')
     } */
-    
+
     if (VERBOSE) { // other checks, these are only run in verbose mode
       if (this.countActors('coin') !== 0) {
         console.log('Coins detected')
       } else {
-        console.warn('This level has no coins!')
+        console.warn('This level has no coins!') // might have to change this later for cut scenes
       }
-      
+
       if (this.countActors('heart') !== 0) {
         console.log('Hearts Detected')
       } else {
